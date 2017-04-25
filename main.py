a2abe5f58eda831e20ef57eac794250b843410bf
Maq301 <franciscoklf@hotmail.com>
2017-04-25 20:11:41 -0300

    Acabo de hacer el menuInicio.py

---
+++ main.py
@@ -0,0 +1,3 @@
+import menuInicio
+
+menuInicio.mostrar()
---
+++ menuInicio.py
@@ -0,0 +1,7 @@
+def mostrar():
+    print("Bienvenido a higth ligth ingresa tu nombre: ")
+    nombre = input()
+    jugar = "Inicia el juego"
+    nivel = "Selecciona tu nivel de dificultad"
+    salir = "Estas saliendo del juego"
+    print("Hola " + nombre + " ahora vas a iniciar el juego")
