bbd66e45c465e079fc5fff7f3c425f1296fb9f25
Maq301 <franciscoklf@hotmail.com>
2017-05-02 19:44:56 -0300

    agregue 2 archivos al main pero todavia fallta mucho por pulir.2

--- cuadrante.py
+++ cuadrante.py
@@ -1,18 +1,14 @@
 apagar = "_"
 prender = "O"
-def juego(apagando, prendiendo):
-
-    if apago:
-        print = (apagar)
-    elif prendo:
-        print(prender)
-
-#luz = O
-#apagado = x
-
 print(apagar, prender)
 lista = [prender, apagar, apagar, prender, apagar]
 print(lista)
 print(lista)
 print(lista)
 print(lista)
+print(lista)
+def juego(apagar, prender):
+    if apago:
+        print = (apagar)
+    elif prendo:
+        print(prender)
