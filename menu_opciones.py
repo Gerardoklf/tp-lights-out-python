d54d07d92d9ed930afc94d2667281e6f65eec42e
Maq301 <franciscoklf@hotmail.com>
2017-05-02 20:07:21 -0300

    corregido Correcciones varias.

--- menu_opciones.py
+++ menu_opciones.py
@@ -1,25 +1,28 @@
-print("Bienvendo a ligth out, Elige entre una de estas funciones")
-elegir = ("Iniciar jugada", "instrucciones", "Reanudar juego", "Salir")
-print("1_" + elegir[0])
-print("2_" + elegir[1])
-print("3_" + elegir[2])
-print("4_" + elegir[3])
 
 def elegir():
-    eleccion = input()
-    print("Ingresa tu eleccion:", eleccion)  # Aca llamo a la funcion que iniciael juego")
+
+    print("Bienvendo a ligth out, Elige entre una de estas funciones")
+    opciones = ("Iniciar jugada", "instrucciones", "Reanudar juego", "Salir")
+    print("1_" + opciones[0])
+    print("2_" + opciones[1])
+    print("3_" + opciones[2])
+    print("4_" + opciones[3])
+
+    print("");
+
+    eleccion = input("Ingresa tu eleccion:")
 
     if (eleccion == "1"):
         print("Preciona cualquier tecla para comenzar el juego.")  # Aca llamo a la funcion que explica el juego.
-    if(eleccion == "2"):
+    elif(eleccion == "2"):
         print("Aca tendria que estar las instrucciones que todabia no hice.")
-    if(eleccion == "3"):
+    elif(eleccion == "3"):
         print("No tenes ningura jugada guardada.")# Aca llamo a la funcion que busca la ultima jugada
-                        # del usuario.
     elif(eleccion == "4"):
         print("Regrece")
         print("Gracias por jugar")
+        exit()
     else:
-        print("Tenes que elegir un numero del 1 al 4 segun lo que")
-        print("quieras hacer.")
-print(elegir())
+        print("[OPCION INCORRECTA] Tenes que elegir un numero del 1 al 4 segun lo que quieras hacer.")
+        print("")
+        elegir()
