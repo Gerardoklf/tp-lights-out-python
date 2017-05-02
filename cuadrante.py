06ad73df1b9253e9d0330cdad5772ada55569dfa
Maq301 <franciscoklf@hotmail.com>
2017-05-02 18:08:46 -0300

    estoy salvando el trabajo andtes de  reiniciar el programa

---
+++ cuadrante.py
@@ -0,0 +1,13 @@
+
+def juego(apagando, prendiendo):
+    apagar = "#"
+    prender = "*"
+    if apago:
+        print = (apagar)
+    elif prendo:
+        print(prender)
+lista = ("#""#""#""#""#")
+print(lista)
+print(lista)
+print(lista)
+print(lista)
